64fda0d67157442802bbf7aaa91b9cc278d60dca
Shohanur Rahman <shohanurvu@gmail.com>
2016-10-23 06:54:02 -0700

    new

---
+++ foreachloop/src/foreachloop/ForEachLop.java
@@ -0,0 +1,10 @@
+package foreachloop;
+
+public class ForEachLop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ foreachloop/src/foreachloop/Foreachloop.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package foreachloop;
+
+/**
+ *
+ * @author S R Bishwas
+ */
+public class Foreachloop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int[] ara={1,2,3,4,5,6,87,43,24,5634};
+        
+        for(int t:ara)
+        {
+            System.out.println(t);
+        }
+    }
+    
+}
